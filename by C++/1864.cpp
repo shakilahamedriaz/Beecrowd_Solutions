19ab029b46c550c6a593d3a14912b77f90f3e0f0
shakilahamedriaz <shakilahamedriaz@gmail.com>
2023-02-07 12:25:05 +0600

    done

---
+++ by C++/1864.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+string s= "LIFE IS NOT A PROBLEM TO BE SOLVED";
+int len= s.size();
+
+int n;
+cin>>n;
+
+
+for(int i=0; i<n; i++)
+{
+
+    cout<<s[i];
+}
+cout<<endl;
+
+
+return 0;
+}
