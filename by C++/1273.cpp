6eab932d7984c4beee9d9f7a37f7146ca763696f
shakilahamedriaz <shakilahamedriaz@gmail.com>
2023-02-14 15:18:51 +0600

    done

---
+++ by C++/1273.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+
+int t;
+while(1){
+cin>>t;
+if(t==0) break;
+
+  while(t--){
+   string s;
+   cin>>s;
+  }
+
+  cout<<s<<endl;
+
+
+}
+
+    return 0;
+
+}
---
+++ by C++/2483.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+
+int n; cin>>n;
+
+char a= 'a';
+
+cout<< "Feliz nat";
+for(int i=0; i<n; i++){
+
+
+
+    cout<<a;
+}
+cout<< "l!"<<endl;
+
+    return 0;
+}
